41ee9c49ea6c8a40440762862d09528a18851d6c
ZALOFARG <andre.work.go@gmail.com>
2024-05-18 22:00:30 -0500

    style: using single quotes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
 const file = process.argv[2];
 
-fs.readFile(file, "utf-8", (error, data) => {
+fs.readFile(file, 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
-    return;
   } else {
     console.log(data);
   }
