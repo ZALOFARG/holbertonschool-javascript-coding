ce4f2cbc826f03ffc378f4988f01693cf62e08ea
ZALOFARG <andre.work.go@gmail.com>
2024-05-31 07:29:03 -0500

    fix: correct error from testing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}\r\n`);
-  process.stdout.write('This important software is now closing\r\n');
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   process.exit();
 });
+
+process.stdin.resume();
